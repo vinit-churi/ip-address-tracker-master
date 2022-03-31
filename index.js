7f223648c063a786f02511d8f0b28dd7b86bcbff
vinit-churi <vinitchuri@hotmail.com>
2022-03-31 13:16:14 +0530

    js functionality remaining

---
+++ index.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  initializeMap();
+});
+
+function initializeMap() {
+  var map = L.map("map").setView([51.505, -0.09], 13);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1Ijoia2V2ZXRpaDg2MSIsImEiOiJja2h4MzFxaG8wOW5pMzBsdGZ1NXFoeHh5In0.hw5mLyF4KWalDgcxAWrmuw",
+    }
+  ).addTo(map);
+  map.zoomControl.setPosition("bottomright");
+}
